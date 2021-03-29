79ba42c4b1b21b5bf0bc553130d0b001ab76e39b
Elvis de Arimatea <elvis.arimatea@outlook.com>
2021-03-29 11:44:51 -0300

    Update Jogo_da_forca.py

--- Jogo_da_forca.py
+++ Jogo_da_forca.py
@@ -1,7 +1,7 @@
 palavra = str(input("Digite a palavra a ser adivinhada: ")).upper()
 forca = '_' * len(palavra)
-forca = list(forca)
-print(' '.join(forca))
+forca = list(forca) #Divide cada caracter de "forca" na string, como se fosse uma lista, com posição
+print(' '.join(forca)) #Une todas as strings colocando ' ' entre elas (' '.join(forca))
 
 resultado = '_'
 mascara = list(palavra)
@@ -15,7 +15,7 @@ while resultado.count('_') > 0 and letra != '0':
         
         for i in range(0, palavra.count(letra)):            
             forca [aux] = letra            
-            aux = palavra.find(letra, aux+1)
+            aux = palavra.find(letra, aux+1) #Procura a proxima ocorrencia da letra em aux+1 (aux+1 porque se não ele acha a mesma letra novamente)
     
     resultado = ' '.join(forca)
     print(resultado)
