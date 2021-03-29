a8e5fcb44463eeeda39ed5ce79938a52eb145f32
Elvis de Arimatea <elvis.arimatea@outlook.com>
2021-03-29 13:02:41 -0300

    Update Jogo_da_forca.py

--- Jogo_da_forca.py
+++ Jogo_da_forca.py
@@ -1,5 +1,8 @@
 from random import randrange
 
+vidas = 5
+win = 1
+
 lista_palavra = open("Palavras_forca.txt", "r")     #Abre o arquivo de palavras e o colocada na variavel "lista_palavra"
 lista_palavra = list(lista_palavra)                 #Divide as palavras em lista
 n_palavra = randrange(0, len(lista_palavra))        #Gera um número aleatório para escolher a palavra
@@ -16,6 +19,7 @@ resultado = '_'
 mascara = list(palavra)
 letra = ''
 
+
 while resultado.count('_') > 0 and letra != '0':
     letra = str(input("Digite uma letra: ")).upper()
     
@@ -25,10 +29,21 @@ while resultado.count('_') > 0 and letra != '0':
         for i in range(0, palavra.count(letra)):            
             forca [aux] = letra            
             aux = palavra.find(letra, aux+1) #Procura a proxima ocorrencia da letra em aux+1 (aux+1 porque se não ele acha a mesma letra novamente)
-    
+    else:
+        vidas -= 1
+        #print(vidas)
+        if vidas <= 0:
+            print("Você perdeu!")
+            win = 0 #auxiliar para não entrar no if do ganhar
+            break
+        elif vidas > 0:
+            #print("Você acaba de perder uma vida!")
+            print("ERROU! Vidas restantes: ", vidas)
+
     resultado = ' '.join(forca)
     print(resultado)
 
-print("Parabéns! Você adivinhou a palavra:", palavra)
+if win != 0:
+    print("Parabéns! Você adivinhou a palavra:", palavra)
     
 
