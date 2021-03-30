65d99b2cec8ada5932efcfa35af0c08ca07ccc8c
Elvis de Arimatea <elvis.arimatea@outlook.com>
2021-03-30 18:20:17 -0300

    Update_Jogo_da_forca

--- Jogo_da_forca.py
+++ Jogo_da_forca.py
@@ -1,9 +1,51 @@
 from random import randrange
 
+#--------------------APRESENTAÇÃO---------------
+print("="*50)
+print("||" + " "*11 + "J O G O   D A   F O R C A" + " " * 10 + "||")
+print("="*50)
+#------------------------------------------------
+
+#--------------------MENU----------------------------
+print("="*50)
+print("|| 1 - Carregar palavras - Nível: Normal" + " "*8 + "||")
+print("|| 2 - Carregar palavras - Nível: Difícil" + " "*7 + "||")
+print("|| 3 - Carregar palavras - Nível: Personalizado" + " "*1 + "||")
+print("="*50)
+#print("|| O que deseja fazer: ")
+selecao = int(input("|| O que deseja fazer: "))
+print("="*50)
+
+
+
+if selecao == 1:            #Dificuldade NORMAL
+    lista_palavra = open("Palavras_forca_normal.txt", "r")     #Abre o arquivo de palavras e o colocada na variavel "lista_palavra"
+    print("-> Arquivo de Palavras Normais carregado!")
+elif selecao == 2:      #Dificuldade DIFÍCIL  
+    lista_palavra = open("Palavras_forca_dificil.txt", "r")     #Abre o arquivo de palavras e o colocada na variavel "lista_palavra"    
+    print("-> Arquivo de Palavras Difíceis carregado!")
+elif selecao == 3:          #Dificuldade PERSONALIZADO 
+    print("|| 1 - Carregar palavras" + " "*24 + "||")
+    print("|| 2 - Adicionar palavras" + " "*23 + "||")
+    print("="*50)
+    selecao = int(input("|| O que deseja fazer: "))
+    print("="*50)
+    if selecao == 1:    #Carregar palavras
+        lista_palavra = open("Palavras_forca_personalizado.txt", "r")        
+        print("-> Arquivo de Palavras Personalizadas carregado!")
+    #elif selecao == 2:  #Editar palavras
+    else:
+        print("Opção inválida!")
+else:
+    print("Opção inválida!")
+
+
+print("\b")
+
+
 vidas = 5
 win = 1
 
-lista_palavra = open("Palavras_forca.txt", "r")     #Abre o arquivo de palavras e o colocada na variavel "lista_palavra"
 lista_palavra = list(lista_palavra)                 #Divide as palavras em lista
 n_palavra = randrange(0, len(lista_palavra))        #Gera um número aleatório para escolher a palavra
 #print(n_palavra)
@@ -40,10 +82,10 @@ while resultado.count('_') > 0 and letra != '0':
             #print("Você acaba de perder uma vida!")
             print("ERROU! Vidas restantes: ", vidas)
 
+    print("\b")
     resultado = ' '.join(forca)
     print(resultado)
 
 if win != 0:
     print("Parabéns! Você adivinhou a palavra:", palavra)
     
-
